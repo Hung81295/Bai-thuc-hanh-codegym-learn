9ad6f0ff097baed8620ecf7e963be3dcbf80b303
LHH812 <lehoanghung1995.6@gmail.com>
2026-09-21 15:08:51 +0700

    xong

---
+++ src/main/java/kiemTraNamNhuan/LeapYearCalculator.java
@@ -0,0 +1,26 @@
+package kiemTraNamNhuan;
+
+import java.util.Scanner;
+
+public class LeapYearCalculator {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int year;
+        System.out.println("Enter the year:");
+        year=input.nextInt();
+        if (year%4==0){
+            if (year%100==0){
+                if (year%400==0){
+                    System.out.println("%d is a leap year"+ year);
+                }else {
+                    System.out.printf("%d is NOT a leap year"+ year);
+
+                }
+            }else{
+                System.out.printf("%d is a leap year"+ year);
+            }
+        }else {
+            System.out.printf("%d is NOT a leap year"+ year);
+        }
+    }
+}
